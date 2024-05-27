3b99b70bde3d6377e851aaa8410d51f5965c076f
Shivraj2609 <shivraj2609@gmail.com>
2024-05-27 12:09:31 +0530

    changes with EC2_Instance_File and Output.tf File

--- EC2_Instance_Create.tf
+++ EC2_Instance_Create.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "EC2_Instance_For_Frontend" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
-  security_groups        = [aws_security_group.instance.name]
+  security_groups        = EC2_Instance_For_Frontend
   count                  = var.instance_count
 
   root_block_device {
--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "instance_id" {
-  value = aws_instance.example.*.id
+  value = aws_instance.EC2_Instance_For_Frontend.*.id
 }
 
 output "public_ip" {
-  value = aws_instance.example.*.public_ip
+  value = aws_instance.EC2_Instance_For_Frontend.*.public_ip
 }
