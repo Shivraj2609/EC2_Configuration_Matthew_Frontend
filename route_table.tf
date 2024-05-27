b8050b742c36b9f53a83ec6884e5545d84e2a899
Shivraj2609 <shivraj2609@gmail.com>
2024-05-27 13:25:34 +0530

    Added

--- route_table.tf
+++ route_table.tf
@@ -12,6 +12,6 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+  subnet_id      = aws_subnet.public-subnet.id
   route_table_id = aws_route_table.public.id
 }
