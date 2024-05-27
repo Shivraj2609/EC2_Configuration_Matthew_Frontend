558ca4fdaef43c2193d425c5b9900d1e7106cb62
Shivraj2609 <shivraj2609@gmail.com>
2024-05-27 13:08:33 +0530

    changes with name at EC2_Instance

--- EC2_Instance_Create.tf
+++ EC2_Instance_Create.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "EC2_Instance_For_Frontend" {
+resource "aws_instance" "example" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "instance_id" {
-  value = aws_instance.EC2_Instance_For_Frontend.*.id
+  value = aws_instance.example.*.id
 }
 
 output "public_ip" {
-  value = aws_instance.EC2_Instance_For_Frontend.*.public_ip
+  value = aws_instance.example.*.public_ip
 }
