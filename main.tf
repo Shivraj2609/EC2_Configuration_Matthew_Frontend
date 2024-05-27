1855b7ba58a39e11f6c40b7aadf6cd13f1b969c3
Shivraj2609 <shivraj2609@gmail.com>
2024-05-27 11:14:39 +0530

    Added Network Conf

--- main.tf
+++ main.tf
@@ -3,12 +3,11 @@ resource "aws_instance" "example" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   security_groups        = [aws_security_group.instance.name]
+  count                  = var.instance_count
 
   root_block_device {
     volume_size = 20
   }
 
-  tags = {
-    Name = "InstanceWithNetworking"
-  }
+  tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,24 +1,50 @@
 variable "region" {
   description = "The AWS region to deploy the resources in"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
   description = "The CIDR block for the public subnet"
+  type        = string
   default     = "10.0.1.0/24"
 }
 
 variable "ami" {
   description = "The AMI to use for the instance"
-  default     = "ami-0e001c9271cf7f3b9"  
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"  # Change to your desired AMI ID
 }
 
 variable "instance_type" {
   description = "The type of instance to use"
+  type        = string
   default     = "t3.medium"
+}
+
+variable "instance_count" {
+  description = "The number of instances to create"
+  type        = number
+  default     = 1
+}
+
+variable "tags" {
+  description = "A map of tags to assign to resources"
+  type        = map(string)
+  default     = {
+    Environment = "dev"
+    Project     = "example"
+  }
+}
+
+variable "allowed_ports" {
+  description = "List of allowed ports"
+  type        = list(number)
+  default     = [22, 80, 443, 3000]
 }
